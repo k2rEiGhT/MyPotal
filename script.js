37ed3dc84ade35eee20273a66f37372a8c2fe973
馬場康二朗 <KOUJIROU@Mark-.local>
2026-03-21 00:02:38 +0900

    update

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const defaultLinks = [
     { title:"Amazon", url:"https://www.amazon.co.jp/","icon":"logo/default.png"},
     { title:"メルカリ", url:"https://jp.mercari.com/","icon":"logo/default.png"},
     { title:"関西電力", url:"https://kepco.jp/miruden/","icon":"logo/default.png"},
-    { title:"タスク管理", url:"readsheets/d/148h01Yy04lEpBxf5m5G_r8EBkMKnpclip1jsp9zeIu4/edit?gid=0#gid=0","icon":"logo/default.png"}
+    { title:"タスク管理", url:"https://docs.google.com/spreadsheets/d/148h01Yy04lEpBxf5m5G_r8EBkMKnpclip1jsp9zeIu4/edit?gid=0#gid=0","icon":"logo/default.png"}
 ];
 
 
